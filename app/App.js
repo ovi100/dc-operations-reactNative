29956464616fc6794274750795ab69a9fcc140e6
ovi100 <ovi0167@hotmail.com>
2024-01-25 11:05:12 +0600

    [Update] boilerplate text

--- app/App.js
+++ app/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+import { SafeAreaView, StatusBar, Text, View } from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView className="p-5">
       <StatusBar barStyle="light-content" />
-      <View className="mt-5">
+      <View className="h-full items-center justify-center mt-5">
         <Text className="text-lg text-gray-400 text-center font-bold">
-          Hello React native
+          React native 0.73.2
+        </Text>
+        <Text className="text-sm text-center font-bold my-5">with</Text>
+        <Text className="text-lg text-gray-400 text-center font-bold">
+          Tailwind CSS 3.3.2
         </Text>
       </View>
     </SafeAreaView>
